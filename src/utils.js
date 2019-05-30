9edbed5264ed8ec4f4a9a492fdf6234274a7759d
lemengbin <lemengbin@163.com>
2019-05-30 19:34:52 +0800

    change isAddress

--- src/utils.js
+++ src/utils.js
@@ -86,15 +86,7 @@ var toTwosComplement = function (number) {
  */
 var isAddress = function (address) {
     // check if it has the basic requirements of an address
-    if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
-        return false;
-        // If it's ALL lowercase or ALL upppercase
-    } else if (/^(0x|0X)?[0-9a-f]{40}$/.test(address) || /^(0x|0X)?[0-9A-F]{40}$/.test(address)) {
-        return true;
-        // Otherwise check each case
-    } else {
-        return checkAddressChecksum(address);
-    }
+    return (/^(Um)?[1-9a-z]{33}$/i.test(address)) && (!/[OIl]{1}/.test(address));
 };
 
 
