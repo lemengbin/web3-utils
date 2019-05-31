1bbcf34b3e340201f0c2fb2ae3d0df1c94b6de87
lemengbin <lemengbin@163.com>
2019-05-31 18:52:15 +0800

    toChecksumAddress

--- src/index.js
+++ src/index.js
@@ -282,25 +282,7 @@ var toWei = function(number, unit) {
  */
 var toChecksumAddress = function (address) {
     if (typeof address === 'undefined') return '';
-
-    if(!/^(0x)?[0-9a-f]{40}$/i.test(address))
-        throw new Error('Given address "'+ address +'" is not a valid Ethereum address.');
-
-
-
-    address = address.toLowerCase().replace(/^0x/i,'');
-    var addressHash = utils.sha3(address).replace(/^0x/i,'');
-    var checksumAddress = '0x';
-
-    for (var i = 0; i < address.length; i++ ) {
-        // If ith character is 9 to f then make it uppercase
-        if (parseInt(addressHash[i], 16) > 7) {
-            checksumAddress += address[i].toUpperCase();
-        } else {
-            checksumAddress += address[i];
-        }
-    }
-    return checksumAddress;
+    return address;
 };
 
 
